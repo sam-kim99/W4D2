7379c93198dffd677f42c814c4187bf3bdef36d7
acheung-94 <acheung@ucdavis.edu>
2024-01-30 13:08:17 -0800

    define bonus methods for both Manager and Employee classes

--- company.rb
+++ company.rb
@@ -1,4 +1,5 @@
 class Employee
+    attr_reader :salary, :title, :name, :boss
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
@@ -12,12 +13,30 @@ class Employee
 end
 
 class Manager < Employee
+    attr_accessor :employees
     def initialize(name, title, salary, boss)
         super
         @employees = []
     end
 
     def bonus(multiplier)
-        bonus
+        sum = 0
+        line = []
+        line.push(*@employees)
+        # = [employee1, employee2, etc] or, employees.dup
+        until line.empty?
+            current = line.shift
+            sum += current.salary
+            line.concat(current.employees) if current.is_a?(Manager)
+        end
+        sum * multiplier
     end
 end
+
+ned = Manager.new('ned', 'big boss', 500, nil)
+darren = Manager.new('darren', 'mid boss', 100, 'ned')
+shawna = Employee.new('shawna', 'little', 20, 'darren')
+david = Employee.new('david', 'little', 20, 'darren')
+ned.employees << darren
+darren.employees << shawna << david
+p ned.bonus(5)
